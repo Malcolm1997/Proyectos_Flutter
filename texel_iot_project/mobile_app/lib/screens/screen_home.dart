df9c59fa41ea9d3ad8eed590f99d8c5b7159315a
Malcolm Gibbons <malcolm.gibbons1997@gmail.com>
2025-12-31 11:49:58 -0300

    feat: add initial home screen UI with active devices and session panels.

--- texel_iot_project/mobile_app/lib/screens/screen_home.dart
+++ texel_iot_project/mobile_app/lib/screens/screen_home.dart
@@ -13,83 +13,122 @@ class _ScreenHomeState extends State<ScreenHome> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Inicio', style: TextStyles.title)),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    left: 10.0,
-                    right: 5.0,
-                    bottom: 5.0,
-                    top: 20.0,
-                  ),
-
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            "2",
-                            style: TextStyles.textBoldTitle,
-                            textAlign: TextAlign.start,
-                          ),
-                          Text(
-                            'Dispositivos Conectados',
-                            style: TextStyles.text,
-                            textAlign: TextAlign.start,
-                          ),
-                          // Aquí iría la lista de dispositivos conectados
-                        ],
-                      ),
-                    ),
-                  ),
+      body: Container(
+        decoration: BoxDecoration(color: Colors.grey[200]),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                panel(
+                  "2",
+                  Icons.published_with_changes_outlined,
+                  "Dispositivos Activos",
+                  Colors.green,
                 ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    left: 5.0,
-                    right: 10.0,
-                    bottom: 5.0,
-                    top: 20.0,
-                  ),
-
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            "2",
-                            style: TextStyles.textBoldTitle,
-                            textAlign: TextAlign.left,
-                          ),
-                          Text(
-                            'Dispositivos en Linea',
-                            style: TextStyles.text,
-                            textAlign: TextAlign.left,
-                          ),
-                          // Aquí iría la lista de dispositivos conectados
-                        ],
-                      ),
-                    ),
-                  ),
+                panel(
+                  "2",
+                  Icons.event_note_outlined,
+                  "Proximas Sesiones",
+                  Color(0xffF59E0B),
                 ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 10.0,
+                right: 10.0,
+                bottom: 10.0,
+                top: 20.0,
               ),
-            ],
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.monitor_heart_outlined),
+                      Text("Dispositivos Activos", style: TextStyles.textBold),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Icon(Icons.book_online),
+                                      Text("Preso"),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Icon(Icons.wifi),
+                                      Text("Online"),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 30),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Text("Ver"),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Expanded panel(String number, IconData icon, String title, Color color) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 10.0,
+          right: 10.0,
+          bottom: 10.0,
+          top: 20.0,
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: Colors.grey),
           ),
-        ],
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Icon(icon, color: color),
+                    SizedBox(width: 10),
+                    Text(number, style: TextStyles.textBoldTitle),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(children: [Text(title, style: TextStyles.text)]),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
